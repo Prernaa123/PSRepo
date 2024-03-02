6f41b0796aec7442f1c3d51eb348d12dc71826e6
prernaa123 <prernak044@gmail.com>
2024-03-02 12:01:41 +0530

    added success

---
+++ BookPageApp/src/main/java/org/ps/bookpageapp/BookPageAppApplication.java
@@ -0,0 +1,13 @@
+package org.ps.bookpageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPageAppApplication.class, args);
+	}
+
+}
---
+++ BookPageApp/src/test/java/org/ps/bookpageapp/BookPageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ps.bookpageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
